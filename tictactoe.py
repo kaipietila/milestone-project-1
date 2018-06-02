b6c6c4da3a835a3ecdee6c834a88f7e7e187a556
kaipietila <kai.m.pietila@gmail.com>
2018-06-02 15:48:00 +0300

    added replayability

--- tictactoe.py
+++ tictactoe.py
@@ -9,9 +9,9 @@ def play_game():
     while winning_condition != True:
         print_grid(results_list)
         if player_one_turn and moves<9:
-            input_player1 = int(input("Player1: Where do you wanna place your mark?"))
+            input_player1 = int(input("Player1: Where do you wanna place your mark? "))
             if input_player1 in inputlist1 or input_player1 in inputlist2:
-                print("this square is already chosen, pick again!")
+                print("This square is already chosen, pick again!")
                 continue
             else:
                 inputlist1.append(input_player1) #store the input and display the grid
@@ -24,9 +24,9 @@ def play_game():
                 
                 
         elif not player_one_turn and moves<9:
-            input_player2 = int(input("Player 2: Where do you wanna place your mark?"))
+            input_player2 = int(input("Player 2: Where do you wanna place your mark? "))
             if input_player2 in inputlist1 or input_player2 in inputlist2:
-                print("this square is already chosen, pick again!")
+                print("This square is already chosen, pick again!")
                 continue
             else:
                 inputlist2.append(input_player2)
@@ -44,6 +44,7 @@ def play_game():
     else:
         print_grid(results_list)
         print("YOU WIN! You got 3 in a row!")
+        play_again()
         
 def print_grid(results_list):
     print("{0} | {1} | {2}\n__|___|___\n{3} | {4} | {5}\n__|___|___\n{6} | {7} | {8}".format(results_list[0],results_list[1],results_list[2],results_list[3],results_list[4],results_list[5],results_list[6],results_list[7],results_list[8]))
@@ -69,15 +70,18 @@ def to_win(inputlist):
     if a:
         return True
         
-            
-        
+def play_again():
+    re_play_question= input("Do you want to play again? Y/N :")
+    if re_play_question.lower() == 'y':
+        play_game()
+    
         
 def game_rules():
     #rules of the game are written here
-    print("Welcome to tic tac toe, go! You aim is to get 3 in a row. Player 1 will use the X marks and player 2 will be O.")
+    print("Welcome to tic tac toe, go! Your aim is to get 3 in a row. Player 1 will use the X marks and player 2 will be O.")
     print("You will assign which square you want to put your mark in by typing in a number between 1-9 in the following order:")
     print("1 | 2 | 3\n__|___|___\n4 | 5 | 6\n__|___|___\n7 | 8 | 9")
     
-
 game_rules()
 play_game()
+
